33dfcea1bc3659d4a5988be8eab7682cab610ae9
Xetr0x <kwilusz10@gmail.com>
2025-08-25 02:49:39 +0200

    fixed a bug

--- server/index.js
+++ server/index.js
@@ -3,15 +3,32 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routes.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const PORT = process.env.PORT || 5000;
-
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api", router);
+app.use("/", router);
+
+const reactBuildPath = path.join(__dirname, "../client/dist");
+app.use(express.static(reactBuildPath));
+
+console.log("React build path:", path.join(reactBuildPath, "index.html"));
+console.log(
+  "Current directory:",
+  "C:/uppgifter/Fullstack_Labs/Fullstack_Lab2/client/dist/index.html"
+);
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(reactBuildPath, "index.html"));
+});
 
 mongoose
   .connect(process.env.CONNECTION_URL)
